336da1b39b3944e44f683324ea6a58956daaeb57
Krishna Adhikari <krishna.forkhaki@gmail.com>
2023-03-25 10:33:54 +0545

    hive updated

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class RegisterController extends GetxController {
-  //TODO: Implement RegisterController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class RegisterController extends GetxController {}
